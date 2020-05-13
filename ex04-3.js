359d24037e6046ec30f53affb7d483f7374e5ac1
shota <koutasukun@icloud.com>
2020-05-13 23:18:38 +0900

    no message

---
+++ ex04-3.js
@@ -0,0 +1,43 @@
+const scores = [13, 67, 18, 54, 30, 22, 89, 49, 21, 65]
+
+
+//function scorenig (score) {
+const score = Math.random() * 100
+
+console.log(`点数:＄{score}`)
+
+if (score >= 13) {
+    console.log('13')
+} else
+if (score >= 67) {
+    console.log('67')
+} else
+if (score >= 18) {
+    console.log('18')
+} 
+if (score >= 54) {
+    console.log('54')
+} 
+if (score >= 30) {
+    console.log('30')
+} 
+if (score >= 22) {
+    console.log('22')
+} 
+if (score >= 89) {
+    console.log('89')
+} 
+if (score >= 49) {
+    console.log('49')
+} 
+if (score >= 21) {
+    console.log('21')
+} 
+if (score >= 65) {
+    console.log('65')
+} 
+//}
+
+for (let scores = 0; scores < 10; scores++) {
+    console.log(scores)
+} 
---
+++ main.js
@@ -0,0 +1,95 @@
+const num = Math.random()
+
+
+
+// console.log(num > 0.5)
+//if(num > 0.5) {
+   // console.log('大きめ')
+//}　else {
+   // console.log('小さめ')
+//}
+
+//console.log(`数: ${num}`)
+
+
+
+// if() {
+   //  console.log('truthy')
+// } else {
+//  console.log('falsy')
+// }
+
+
+const a = 2
+const b = 3
+const c = 4
+const d = 5
+
+console.log(a === b)
+console.log(a !== b)
+console.log(a < b)
+console.log(a <= b)
+console.log(a > b)
+console.log(a >= b)
+
+
+console.log(a === b && c === b)
+console.log(a === b || c === b)
+console.log(!(a === b))
+
+
+console.log(1 === "1")
+console.log(1 == "1")
+
+const score = Math.random() * 100
+
+console.log(`得点: &{srore}`)
+
+if(score >= 30) {
+    console.log('可')
+} else
+if(score >= 90) {
+    console.log('秀')
+} else 
+if(score >= 75) {
+    console.log('優')
+} else
+if(score >= 60) {
+    console.log('良') 
+} else {
+    console.log('不可')
+}
+
+//for(let i = 0; i < 10; i++) {
+   // console.log(i)
+//}
+
+let s = 2
+
+while (s < 100000) {
+    console.log(s)
+    s = s * s
+}
+
+const friends = ['サーバル', 'フェネック', 'アライグマ']
+
+friends.forEach((friends) => {
+    console.log(friends)
+})
+
+
+function cook(food1, food2) {
+    console.log(food1 + 'を切ります。')
+    console.log(food1 + 'を炒めます。')
+    console.log(food2 + 'をすりおろします。')
+    console.log(food1 + 'に' + food2 + 'を混ぜます。')
+}
+cook('豚肉', '生姜')
+
+
+function applyTax(price) {
+    const result = price * 0.1
+    return result
+}
+const priceApplyTax = 1000 + applyTax(1000)
+console.log(priceApplyTax)
